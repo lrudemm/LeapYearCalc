b39dddcbb3aebd8aff1534ca39ba172bf9ade3f4
Larry R <69208186+lrudemm@users.noreply.github.com>
2022-03-11 13:56:12 +0000

    I shortened JS answers. Added a gradient.

--- script.js
+++ script.js
@@ -1,9 +1,9 @@
 let year = prompt("What year would you like to check?")
 
 if (year % 4 === 0 && year % 100 !== 0) {
-    console.log(`The year ${year} is a Leap year`);
+    console.log(`${year} is a Leap year!`);
 } else if (year % 4 === 0 && year % 400) {
-    console.log(`The year ${year} is a Leap year`);
+    console.log(`${year} is a Leap year!`);
 } else {
-    console.log(`The year ${year} is not a Leap year`);
+    console.log(`${year} is not a Leap year. ):`);
 }
